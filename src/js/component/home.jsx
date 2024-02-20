b85da5158c41bd091ea5f16db65707aa73c9ee8b
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-02-20 03:03:02 +0000

    :tada: todo list started (no css included)

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,38 @@
-import React from "react";
-
+import React, {useState} from "react";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [todo, setTodo] = useState([])
+
+	const deleteTask = (taskName)=>{
+		const todoFilter = todo.filter( itm => itm != taskName )
+		setTodo(todoFilter)
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid d-flex justify-content-center flex-column w-50">
+			<h1 className="align-self-center">todos</h1>
+			<input className="input-el" type="text" placeholder="Escribe la tarea aca"
+				onKeyDown={(event)=>{
+					if(event.key == "Enter" && event.target.value != ""){
+						setTodo([...todo, event.target.value])
+						event.target.value = ""
+					}
+				}}
+			/>
+
+			{
+				todo.map((task, i)=> {
+					return (
+						<div key={i} className="tarea border border-top-0 d-flex justify-content-between align-items-center">
+							<p className="m-0">{task}</p>
+							<button className="border-0" type="button" onClick={()=> deleteTask(task)}>x</button>
+						</div>
+					)
+				})
+			}
 		</div>
 	);
 };
